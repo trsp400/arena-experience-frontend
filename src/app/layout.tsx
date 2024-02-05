62c8c0fdd64b32aeff6b59c7f4369a5cc96cce04
Thiago Robles <roblesthiago38@gmail.com>
2024-02-05 00:15:54 -0300

    fix build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import { cn } from "@/lib/utils"
 import Header from "@/components/layout/header";
 import { Toaster } from "@/components/ui/toaster";
 
-export const fontSans = FontSans({
+const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 })
